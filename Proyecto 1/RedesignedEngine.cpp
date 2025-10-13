3c688ccf4bb42cb69c80607a7d49630cb53b4179
CebGIN <cebgin1@gmail.com>
2025-10-12 21:27:29 -0400

    Changes to the tree life cycle

---
+++ Proyecto 1/RedesignedEngine.cpp
@@ -0,0 +1,185 @@
+#include <functional>
+#include <windows.h>
+#include <iostream>
+#include <tchar.h>
+#include <memory>
+#include <vector>
+#include <string>
+
+class Node : public std::enable_shared_from_this<Node>{
+    protected:
+        std::vector<std::shared_ptr<Node>> Childs;
+        std::weak_ptr<Node> Parent;
+        std::string name;
+        std::function<void()> process;
+        std::function<void()> at_enter_tree;
+        std::function<void()> at_exit_tree;
+    
+    public:
+        Node(const std::string& nodeName = "Unnamed Node") : name(nodeName) {
+            // std::cout << "Node '" << name << "' created." << std::endl;
+            process = [](){ /* Do nothing by default */ };
+        }
+        
+        virtual ~Node() {
+            // std::cout << "Node '" << name << "' destroyed." << std::endl;
+        }
+        
+        void setParent(std::shared_ptr<Node> parentNode) {
+            Parent = parentNode;
+        }
+        
+        std::shared_ptr<Node> getParent() const {
+            return Parent.lock();
+        }
+        
+        bool isRoot() const {
+            return Parent.expired() || !Parent.lock();
+        }
+        
+        void addChild(std::shared_ptr<Node> childNode) {
+            if (childNode) {
+                Childs.push_back(childNode);
+                childNode->setParent(shared_from_this());
+            } 
+            else {
+                std::cerr << "Warning: Attempted to add a null shared_ptr child node." << std::endl;
+            }
+        }
+    
+        size_t getChildCount() const {
+            return Childs.size();
+        }
+    
+        std::shared_ptr<Node> getChild(size_t index) {
+            if (index < Childs.size()) {
+                return Childs[index];
+            } else {
+                throw std::out_of_range("Node::getChild(index): Index out of bounds.");
+            }
+        }
+    
+        std::shared_ptr<const Node> getChild(size_t index) const {
+            if (index < Childs.size()) {
+                return Childs[index];
+            } else {
+                throw std::out_of_range("Node::getChild(index): Index out of bounds.");
+            }
+        }
+    
+        std::vector<std::shared_ptr<Node>>::iterator begin() { return Childs.begin();}
+        std::vector<std::shared_ptr<Node>>::iterator end() { return Childs.end();}
+    
+        std::vector<std::shared_ptr<Node>>::const_iterator begin() const { return Childs.cbegin();} // Use cbegin for const access
+        std::vector<std::shared_ptr<Node>>::const_iterator end() const { return Childs.cend();}     // Use cend for const access
+    
+        const std::string& getName() const {
+            return name;
+        }
+    
+        virtual std::string getType() const {
+            return "Node";
+        }
+    
+        virtual void update() {
+            // 1. Ejecutar la lógica de proceso de este nodo
+            process(); 
+    
+            // 2. Propagar la llamada a update a todos los hijos
+            for (const auto& child : Childs) {
+                child->update(); 
+            }
+        }
+        virtual void enter_tree() {
+            // 1. Ejecutar la lógica de entrada de este nodo
+            at_enter_tree(); 
+            
+            // 2. Propagar la llamada a   a todos los hijos
+            for (const auto& child : Childs) {
+                child->enter_tree(); 
+            }
+        }
+        virtual void exit_tree() {
+            // 1. Propagar la llamada a salir a todos los hijos
+            for (const auto& child : Childs) {
+                child->exit_tree(); 
+            }
+            // 2. Ejecutar la lógica de salida de este nodo
+            at_exit_tree(); 
+        }
+        void setProcessFunction(std::function<void()> func) {
+            if (func) {
+                process = func;
+            } else {
+                process = [](){}; 
+                // std::cerr << "Warning: Attempted to set a null process function for Node '" << name << "'." << std::endl;
+            }
+        }
+    
+        // virtual void draw(ConsoleRenderer& renderer) {
+        // for (const auto& child : Childs) {
+        //     child->draw(renderer); // Propagar la llamada a draw a los hijos
+        // }
+        // }
+    
+        void printHierarchy(int indentLevel = 0) const {//debug
+            std::cout << std::string(indentLevel * 4, ' ');
+            std::cout << "- " << name << " (" << getType() << ")"; // Print node type
+            if (isRoot()) {
+                std::cout << " (Root)";
+            }
+            std::cout << std::endl;
+    
+            for (const auto& child : Childs) {
+                child->printHierarchy(indentLevel + 1);
+            }
+        }
+    };
+    
+    class Node2D : public Node{
+    protected:
+        COORD position;
+        public:
+        
+        Node2D(const std::string& nodeName = "Unnamed Node", COORD nodePosition = {0, 0}) : Node(nodeName), position(nodePosition){}
+    
+    
+        COORD getLocalPosition() const {
+            return position;
+        }
+        void setLocalPosition(COORD new_position){
+            position = new_position;
+            // std::cout << "Node2D '" << name << "' local position set to (" << position.X << ", " << position.Y << ")." << std::endl;
+        }
+    
+        std::string getType() const override {
+            return "Node2D";
+        }
+    
+        COORD getGlobalPosition() const {
+            COORD globalPos = position;
+    
+            std::shared_ptr<Node> currentParent = getParent(); 
+            while (currentParent) {
+                std::shared_ptr<Node2D> parent2D = std::dynamic_pointer_cast<Node2D>(currentParent);
+                if (parent2D) {
+                    globalPos = addCoords(parent2D->position, globalPos);
+                }
+                currentParent = currentParent->getParent();
+            }
+            return globalPos;
+        }
+        
+        void setGlobalPosition(COORD newGlobalPosition) {
+            COORD currentGlobalPosition = getGlobalPosition(); // G
+            COORD currentLocalPosition = getLocalPosition();   // L
+    
+            COORD newLocalPosition = addCoords(newGlobalPosition, currentLocalPosition); // Gn + L
+            newLocalPosition = subtractCoords(newLocalPosition, currentGlobalPosition);  // (Gn + L) - G
+    
+            setLocalPosition(newLocalPosition);
+            // std::cout << "Node2D '" << name << "' global position set to ("
+            //           << newGlobalPosition.X << ", " << newGlobalPosition.Y << ")"
+            //           << " (New Local: " << newLocalPosition.X << ", " << newLocalPosition.Y << ")." << std::endl;
+        }
+    };
