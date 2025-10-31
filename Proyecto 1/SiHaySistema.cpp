a980586c26418e0f4513682633953de6de465f2e
CebGIN <cebgin1@gmail.com>
2025-10-30 22:31:13 -0400

    Added data Structures

--- Proyecto 1/SiHaySistema.cpp
+++ Proyecto 1/SiHaySistema.cpp
@@ -1,5 +1,6 @@
+#include<cassert>
+#include<cstring>
 #include "LegacyEngine.hpp"
-
 
 template<typename T> class LinkedList;
 
@@ -7,12 +8,12 @@ template<typename T>
 class ListElement{
     friend  class LinkedList<T>;
     private:
-    ListElement<T>* prev;
+    ListElement<T>* prev = nullptr;
     T value;
-    ListElement<T>* next;
+    ListElement<T>* next = nullptr;
     
     ListElement(T tvalue, ListElement<T>* previous = nullptr, ListElement<T>* next_ptr = nullptr)
-    : value(tvalue), prev(previous), next(next_ptr) {}
+    : prev(previous), value(tvalue), next(next_ptr) {}
 };
 template<typename T>
 class LinkedList{
@@ -22,7 +23,7 @@ class LinkedList{
     public:
     LinkedList() : size(0), start(nullptr), end(nullptr) {}
 
-    int get_size() cont {
+    int get_size() const {
         return size;
     }
     
@@ -51,7 +52,76 @@ class LinkedList{
         }
         size++;
     }
-
-
-    
 };
+
+struct Appointment {
+    int id;
+    int patientId;
+    int doctorId;
+    char date[11];
+    char time[6];
+    char reason[150];
+    char status[20];
+    char notes[200];
+    bool wasAttended;
+};
+
+struct Doctor {
+    int id;
+    char firstName[50];
+    char lastName[50];
+    char nationalId[20];
+    char specialty[50];
+    int yearsExperience;
+    float consultationFee;
+    char workingHours[50];
+    char phone[15];
+    char email[50];
+    
+    LinkedList<Appointment*> scheduledAppointments;
+    
+    bool isAvailable;
+};
+
+struct MedicalRecord {
+    int recordId;
+    char date[11];
+    char time[6];
+    char diagnosis[200];
+    char treatment[200];
+    char medications[150];
+    int doctorId;
+    float cost;
+};
+
+struct Patient {
+    int id;
+    char firstName[50];
+    char lastName[50];
+    char nationalId[20];
+    int age;
+    char gender;
+    char bloodType[5];
+    char phone[15];
+    char address[100];
+    char email[50];
+    
+    LinkedList<MedicalRecord*> history;
+    LinkedList<Appointment*> scheduledAppointments;
+    
+    char allergies[500];
+    char notes[500];
+    
+    bool isActive;
+};
+
+struct Hospital {
+    char name[100];
+    char address[150];
+    char phone[15];
+    
+    LinkedList<Patient*> patients;
+    LinkedList<Doctor*> doctors;
+    LinkedList<Appointment*> appointments;
+};
+
